c4028dbf2c689069409ddaa7257276fc50142d57
EstherKiguru <esipraisewambui@gmail.com>
2024-08-01 22:47:22 +0300

    get logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,9 @@ import logging
 import re
 
 
+PII_FIELDS = ("email", "phone", "ssn", "password", "name")
+
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """ Obfuscates the log message """
@@ -32,3 +35,15 @@ class RedactingFormatter(logging.Formatter):
             self.fields, self.REDACTION, super(
                 RedactingFormatter, self).format(record),
             self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """creates and configures a logger"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
