511a16f168e8dd20377a9e91b7b25dc7670d8042
EstherKiguru <esipraisewambui@gmail.com>
2024-08-01 22:08:31 +0300

    obfuscation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ import re
 
 def filter_datum(fields: list[str], redaction: str, message: str,
                  separator: str) -> str:
-    """ Replacing """
+    """ Obfuscates the log message """
     for f in fields:
         message = re.sub(rf"{f}=(.*?)\{separator}",
                          f'{f}={redaction}{separator}', message)
