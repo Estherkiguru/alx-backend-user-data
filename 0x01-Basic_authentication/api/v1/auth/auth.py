44660036d6fd64695837ecff6cce9c8bd43337f4
EstherKiguru <esipraisewambui@gmail.com>
2024-08-08 18:49:07 +0300

    Authorized class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Auth class"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ returns False - path"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Flask object that returns None - request"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Flask request object that returns None - request"""
+        return None
