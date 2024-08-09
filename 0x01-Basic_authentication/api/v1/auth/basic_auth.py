c2c21e831bdf36ceef606cf824b7f252665bebe2
EstherKiguru <esipraisewambui@gmail.com>
2024-08-09 23:13:12 +0300

    Extracting user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -35,3 +35,20 @@ class BasicAuth(Auth):
             return decoded_str
         except Exception as e:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header:
+            str) -> (str, str):
+        """Handles user email and password"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        try:
+            email, password = decoded_base64_authorization_header.split(':', 1)
+            return email, password
+        except Exception as e:
+            return None, None
