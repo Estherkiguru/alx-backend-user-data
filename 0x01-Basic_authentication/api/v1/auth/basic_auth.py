7e707d124cac7fa28babbf2f861751da185aca6e
EstherKiguru <esipraisewambui@gmail.com>
2024-08-09 23:34:51 +0300

    User instances

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,8 @@
 Basic Auth module
 """
 from api.v1.auth.auth import Auth
-import binascii
+from typing import TypeVar
+from models.user import User
 import base64
 
 
@@ -52,3 +53,23 @@ class BasicAuth(Auth):
             return email, password
         except Exception as e:
             return None, None
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """User credentials based on email and password"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        
+        Users = User.search(user_email)
+        print(f"Users found: {users}")
+
+        if not Users:
+            return None
+        
+        user = Users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
+
