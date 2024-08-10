387692c17d7f8ef066c8c3c3a97251ede09f486c
EstherKiguru <esipraisewambui@gmail.com>
2024-08-10 22:48:32 +0300

    before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,12 +54,10 @@ def before_request():
                        '/api/v1/auth_session/login/']
 
     if auth and auth.require_auth(request.path, authorized_path):
-        if not auth.authorization_header(request):
+        if not auth.authorization_header(request) and not auth.session_cookie(request):
             abort(401)
         if not auth.current_user(request):
             abort(403)
-        if not auth.session_cookie(request):
-            abort(401)
         request.current_user = auth.current_user(request)
 
 
