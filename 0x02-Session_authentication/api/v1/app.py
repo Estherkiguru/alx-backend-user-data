36912ff2f9d01070ff56dc0a9524cd2ace94b6ee
EstherKiguru <esipraisewambui@gmail.com>
2024-08-10 21:59:48 +0300

    session auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from os import getenv
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -21,7 +22,8 @@ if getenv("AUTH_TYPE") == "auth":
     auth = Auth()
 elif getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
-
+elif getenv("AUTH_TYPE") == 'session_auth':
+    auth = SessionAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
