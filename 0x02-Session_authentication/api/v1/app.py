bddb835bbd96fdf9ec9ad3c05de44ac1e17ebd10
EstherKiguru <esipraisewambui@gmail.com>
2024-08-10 23:36:59 +0300

    session ID expiration date

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -24,7 +25,8 @@ elif getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
 elif getenv("AUTH_TYPE") == 'session_auth':
     auth = SessionAuth()
-
+elif getenv("AUTH_TYPE") == 'session_exp_auth':
+    auth = SessionExpAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
