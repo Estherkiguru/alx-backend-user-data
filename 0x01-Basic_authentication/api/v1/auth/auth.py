20b0a3624e56b9994c33f6d8a68355973e933095
EstherKiguru <esipraisewambui@gmail.com>
2024-08-10 20:23:24 +0300

    corrections

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,19 +10,21 @@ class Auth:
     """Managed the API authentication."""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ returns False - path"""
-        if path is not None and excluded_paths is not None:
-            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
-                pattern = ''
-                if exclusion_path[-1] == '*':
-                    pattern = '{}.*'.format(exclusion_path[0:-1])
-                elif exclusion_path[-1] == '/':
-                    pattern = '{}/*'.format(exclusion_path[0:-1])
-                else:
-                    pattern = '{}/*'.format(exclusion_path)
-                if re.match(pattern, path):
-                    return False
+        if path is None or excluded_paths is  None:
             return True
 
+        normalized_path = path.rstrip('/') + '/'
+
+        for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+            normalized_exclusion = exclusion_path.rstrip('/') + '/'
+
+            if exclusion_path.endswith('*'):
+                if re.match(f'^{re.escape(normalized_exclusion[:-2])}.*', normalized_path):
+                    return False
+            else:
+                if normalized_path == normalized_exclusion:
+                    return False
+        return True
     def authorization_header(self, request=None) -> str:
         """Flask object that returns None - request"""
         if request is None:
