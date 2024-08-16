68b1ec6cb9e95e382c2b951f93addce125be7998
EStherKiguru <esipraisewambui@gmail.com>
2024-08-16 20:08:42 +0300

    basic flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,12 +5,13 @@ from flask import Flask, request, jsonify, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
-#auth = Auth()
+
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def home() -> str:
     """basic Flask app"""
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
