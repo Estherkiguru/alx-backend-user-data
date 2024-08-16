bc9c3a7b02acfc436572ef6c41f792cb81961b5e
EStherKiguru <esipraisewambui@gmail.com>
2024-08-16 20:28:49 +0300

    basic flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,12 +5,24 @@ from flask import Flask, request, jsonify, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def home() -> str:
     """basic Flask app"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user() -> str:
+    """Endpoint to register a user"""
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"email": email, "message": "email already registered"})
 
 
 if __name__ == "__main__":
