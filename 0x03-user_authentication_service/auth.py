ba40c9114560199c97fb89639602e54cfcca59f3
EStherKiguru <esipraisewambui@gmail.com>
2024-08-16 17:18:46 +0300

    Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Authentication module"""
+
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password and returns bytes"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
